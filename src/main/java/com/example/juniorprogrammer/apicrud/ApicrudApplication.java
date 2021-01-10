ff3a3d1fdad8bd8378ad2d267ff6cb79e6b55ab6
muhammad.tama <direktoratctf@Rustams-MacBook-Pro.local>
2021-01-10 21:05:42 +0700

    base api crud

---
+++ src/main/java/com/example/juniorprogrammer/apicrud/ApicrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.juniorprogrammer.apicrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/juniorprogrammer/apicrud/ApicrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.juniorprogrammer.apicrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

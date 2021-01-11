682f360913b05f8e19b46f56a0aaaca35dc55499
muhammad.tama <direktoratctf@Rustams-MacBook-Pro.local>
2021-01-11 09:05:38 +0700

    end of add-car-api

--- src/main/java/com/example/juniorprogrammer/apicrud/controllers/CarController.java
+++ src/main/java/com/example/juniorprogrammer/apicrud/controllers/CarController.java
@@ -1,15 +1,27 @@
 package com.example.juniorprogrammer.apicrud.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.juniorprogrammer.apicrud.entities.CarEntity;
+import com.example.juniorprogrammer.apicrud.repositories.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "car")
 public class CarController {
 
+    @Autowired
+    CarRepository carRepository;
+
     @GetMapping(value = "checkAPI")
-    public String checkApi(){
+    public String checkApi() {
         return "Hello World";
     }
+
+    @PostMapping(value = "addNewCar")
+    public CarEntity addNewCar(@RequestBody CarEntity param) {
+
+        carRepository.save(param);
+        return param;
+    }
+
 }
